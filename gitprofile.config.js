866d750a566c6d52639cefcf1b1271d51f1579ec
Sanket Disale <109368165+sanketdisale871@users.noreply.github.com>
2023-11-08 14:32:31 +0530

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -7,7 +7,7 @@ const config = {
     limit: 1, // How many projects to display.
     exclude: {
       forks: false, // Forked projects will not be displayed if set to true.
-      projects: [], // These projects will not be displayed. example: ['my-project1', 'my-project2']
+      projects: ['DSA'], // These projects will not be displayed. example: ['my-project1', 'my-project2']
     },
   },
   social: {
